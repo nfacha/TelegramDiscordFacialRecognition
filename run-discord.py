d3acc989f17f8edb7f3fc809aadc6fa8b98ecf3b
Nuno Facha <nunofacha@gmail.com>
2018-05-31 00:38:57 +0000

    Another bug bites the dust

--- run-discord.py
+++ run-discord.py
@@ -28,53 +28,51 @@ async def on_message(message):
 
     :type message: discord.message.Message
     """
-    if Utils.isStaff(
-            message.author) or message.channel.id == "451418762332209153" or message.channel.id == "445990794852302892":
-        if len(message.attachments) != 0:
-            print("File received from %s" % message.author.name)
-            for attachment in message.attachments:
-                print("Downloading file...")
-                msg = await client.send_message(message.channel, "Downloading file...")
-                name = str(attachment['url']).split("/")
-                name = attachment['id'] + "_" + name[len(name) - 1]
-                headers = {
-                    'User-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
-                }
-                response = requests.get(attachment['proxy_url'], stream=True, headers=headers)
-                with open("tmp/" + name, "wb") as handle:
-                    for data in tqdm(response.iter_content()):
-                        handle.write(data)
-                await client.edit_message(msg, "Downloaded as /tmp/" + name + ", processing please wait...")
-                print("File saved as %s" % name)
-                found = False
-                print("Initializing recognition...")
-                for filename in os.listdir("known-person"):
-                    image = face_recognition.load_image_file("known-person/" + filename)
-                    unknown_image = face_recognition.load_image_file("tmp/" + name)
-                    try:
-                        image_encoding = face_recognition.face_encodings(image)[0]
-                        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-                        known_faces = [
-                            image_encoding
-                        ]
-                        results = face_recognition.compare_faces(known_faces, unknown_face_encoding, tolerance=0.6)
+    if len(message.attachments) != 0:
+        print("File received from %s" % message.author.name)
+        for attachment in message.attachments:
+            print("Downloading file...")
+            msg = await client.send_message(message.channel, "Downloading file...")
+            name = str(attachment['url']).split("/")
+            name = attachment['id'] + "_" + name[len(name) - 1]
+            headers = {
+                'User-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+            }
+            response = requests.get(attachment['proxy_url'], stream=True, headers=headers)
+            with open("tmp/" + name, "wb") as handle:
+                for data in tqdm(response.iter_content()):
+                    handle.write(data)
+            await client.edit_message(msg, "Downloaded as /tmp/" + name + ", processing please wait...")
+            print("File saved as %s" % name)
+            found = False
+            print("Initializing recognition...")
+            for filename in os.listdir("known-person"):
+                image = face_recognition.load_image_file("known-person/" + filename)
+                unknown_image = face_recognition.load_image_file("tmp/" + name)
+                try:
+                    image_encoding = face_recognition.face_encodings(image)[0]
+                    unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+                    known_faces = [
+                        image_encoding
+                    ]
+                    results = face_recognition.compare_faces(known_faces, unknown_face_encoding, tolerance=0.6)
 
-                        if results[0]:
-                            print("MATCH // Positive ID against sample known-person/" + filename)
-                            await client.edit_message(msg,
-                                                      ":white_check_mark:  Facial recognition match! -> " + filename)
-                            found = True
-                            break
-                        else:
-                            print("NO MATCH // against sample known-person/" + filename)
+                    if results[0]:
+                        print("MATCH // Positive ID against sample known-person/" + filename)
+                        await client.edit_message(msg,
+                                                  ":white_check_mark:  Facial recognition match! -> " + filename)
+                        found = True
+                        break
+                    else:
+                        print("NO MATCH // against sample known-person/" + filename)
 
-                    except IndexError:
-                        pass
-                if not found:
-                    await client.edit_message(msg, ":question: No facial recognition match found")
-                    print("No results found")
-                print("-------------------")
+                except IndexError:
+                    pass
+            if not found:
+                await client.edit_message(msg, ":question: No facial recognition match found")
+                print("No results found")
+            print("-------------------")
 
 
-def spin():
-    client.run(os.getenv("api-key"))
+print("Aye Aye, spinning up DISCORD bot")
+client.run(os.getenv("api-key"))
--- run-telegram.py
+++ run-telegram.py
@@ -8,7 +8,7 @@ from telegram import Message
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
+                    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +16,10 @@ logger = logging.getLogger(__name__)
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
+
 upd = None  # type: Updater
+
+
 def init():
     import sys
     if sys.version_info[0] < 3:
@@ -30,17 +33,19 @@ def init():
     updater.start_polling()
     print("Bot is listening")
 
-def execute(bot,update):
+
+def execute(bot, update):
     """
 
     :type bot: telegram.bot.Bot
     """
     message = update.message  # type: Message
     print(message)
-    print("Received file from "+message.from_user.username)
-    update.message.reply_text("Please wait, processing...", parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+    print("Received file from " + message.from_user.username)
+    update.message.reply_text("Please wait, processing...", parse_mode=ParseMode.MARKDOWN,
+                              disable_web_page_preview=True)
     file = bot.get_file(update['_effective_message']['photo'][-1]['file_id'])
-    name = "tmp/"+file.file_id+"."+str(file.file_path).split(".")[-1];
+    name = "tmp/" + file.file_id + "." + str(file.file_path).split(".")[-1];
     print(name)
     file.download(name)
     found = False
@@ -72,16 +77,13 @@ def execute(bot,update):
     print("-------------------")
     update.message.reply_text(response, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
-
     pass
 
 
-def spin():
-    init()
-    keepUp = True
-    while keepUp:
-        command = input()
-        if command == "stop":
-            keepUp = False
-
-
+print("Aye Aye, spinning up TELEGRAM bot")
+init()
+keepUp = True
+while keepUp:
+    command = input()
+    if command == "stop":
+        keepUp = False
